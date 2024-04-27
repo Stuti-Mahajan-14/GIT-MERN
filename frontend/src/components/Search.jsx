61ffac7ee171b75ced8a624f76cb393219093868
Stuti-Mahajan-14 <connect.stuti@gmail.com>
2024-04-27 14:33:01 +0530

    1

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -2,7 +2,7 @@ import { IoSearch } from "react-icons/io5";
 
 const Search = () => {
 	return (
-		
+
 		<form className='max-w-xl mx-auto p-2 sm:w-72'>
 			<label htmlFor='default-search' className='mb-2 text-sm font-medium text-gray-900 sr-only'>
 				Search
@@ -22,7 +22,7 @@ const Search = () => {
 					type='submit'
 					className='text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2  bg-gradient-to-r from-cyan-900 to-blue-900 hover:scale-95 active:scale-90 transition-all duration-300'
 				>
-					Search
+					Search it
 				</button>
 			</div>
 		</form>
